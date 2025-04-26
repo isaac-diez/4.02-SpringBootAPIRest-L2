3a14a9f304cbc7e8a39c6d718c8f9fdbbb432f1c
isaac-diez <isaac.diez@proton.me>
2025-04-26 11:46:08 +0200

    Feat: created all structure. Connects to DB but create method fails.

---
+++ src/main/java/cat/itacademy/s04/t02/n02/exception/EmptyFruitListException.java
@@ -0,0 +1,8 @@
+package cat.itacademy.s04.t02.n02.exception;
+
+public class EmptyFruitListException extends RuntimeException {
+    public EmptyFruitListException(String message) {
+
+        super(message);
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+INSERT INTO FRUITS (name,weight) VALUES ('apple',10), ('strawberry',5), ('banana',15), ('lemon',12), ('orange',14);
